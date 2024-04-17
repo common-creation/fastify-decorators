6063506c5b43f0bf3a2d3a5d8eb597370a9f1f1b
mohemohe <mohemohe@ghippos.net>
2024-04-17 15:57:35 +0900

    change log level

--- src/route/index.ts
+++ src/route/index.ts
@@ -18,27 +18,27 @@ export function route(path: string) {
         const normalizedPath = nodePath.posix.join("/", internalPrefix, path);
 
         if (target.get) {
-          server.log.debug({ method: "GET", path: normalizedPath, func: `${target.name}.get` }, "register route");
+          server.log.trace({ method: "GET", path: normalizedPath, func: `${target.name}.get` }, "register route");
           server.get(normalizedPath, target.get);
         }
         if (target.post) {
-          server.log.debug({ method: "POST", path: normalizedPath, func: `${target.name}.post` }, "register route");
+          server.log.trace({ method: "POST", path: normalizedPath, func: `${target.name}.post` }, "register route");
           server.post(normalizedPath, target.post);
         }
         if (target.put) {
-          server.log.debug({ method: "PUT", path: normalizedPath, func: `${target.name}.put` }, "register route");
+          server.log.trace({ method: "PUT", path: normalizedPath, func: `${target.name}.put` }, "register route");
           server.put(normalizedPath, target.put);
         }
         if (target.delete) {
-          server.log.debug({ method: "DELETE", path: normalizedPath, func: `${target.name}.delete` }, "register route");
+          server.log.trace({ method: "DELETE", path: normalizedPath, func: `${target.name}.delete` }, "register route");
           server.delete(normalizedPath, target.delete);
         }
         if (target.head) {
-          server.log.debug({ method: "HEAD", path: normalizedPath, func: `${target.name}.head` }, "register route");
+          server.log.trace({ method: "HEAD", path: normalizedPath, func: `${target.name}.head` }, "register route");
           server.head(normalizedPath, target.head);
         }
         if (target.patch) {
-          server.log.debug({ method: "PATCH", path: normalizedPath, func: `${target.name}.patch` }, "register route");
+          server.log.trace({ method: "PATCH", path: normalizedPath, func: `${target.name}.patch` }, "register route");
           server.patch(normalizedPath, target.patch);
         }
 
